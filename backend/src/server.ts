163bf20805cf3b72015940469eb087c770e591bc
Júlio César <129863610+jcmoreiraa@users.noreply.github.com>
2025-04-08 12:04:56 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://capable-determination-production.up.railway.app/', 
+    origin: 'https://atipicidades-ssks.vercel.app', 
     credentials: true 
 }));
 app.use(cookieParser());
